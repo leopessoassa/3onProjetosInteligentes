96e2bcdb9cfa68fb668de1cdb0183f794e1b90a6
Leonardo Pessoa <leonardo.pmoura@dataprev.gov.br>
2025-07-05 22:08:26 -0300

    alteração do vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import vercel from "vite-plugin-vercel";
-import path from 'path';
 
 export default defineConfig({
   plugins: [vercel(), tailwindcss(), reactRouter(), tsconfigPaths()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: "dist",
-  },
 });
