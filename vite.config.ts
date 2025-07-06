f75486d42c900aca0221e2532ae37f1855a90935
Leonardo Pessoa <leonardo.pmoura@dataprev.gov.br>
2025-07-05 21:28:02 -0300

    config vit

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,21 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import vercel from 'vite-plugin-vercel';
+import vercel from "vite-plugin-vercel";
+import path from 'path';
 
 export default defineConfig({
-  plugins: [vercel(),tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [vercel(), tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "dist",
+  },
 });
